b2d12aefef7ef29a0675761d5418eca5bbdd2ded
Christian Kniep <christian@qnib.org>
2017-04-11 22:35:32 +0200

    Bump version: 0.1.2 → 0.1.3

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "fancy-tool"
 	app.Usage = "This fancy tool is used to improve live."
-	app.Version = "0.1.2"
+	app.Version = "0.1.3"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "config",
